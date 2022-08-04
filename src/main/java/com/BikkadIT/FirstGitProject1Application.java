2703dce23609df16d0012cfdd7facced09ab9fb9
Pravin <pravinbhakar01@gmail.com>
2022-08-04 18:53:30 +0530

    sms 01: Main Project

---
+++ src/main/java/com/BikkadIT/FirstGitProject1Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitProject1Application.class, args);
+	}
+
+}
